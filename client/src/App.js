f9c1e141bdbf24b6def380eb357a76e6ba90147b
Minja Sillanpää <minja@vainu.io>
2020-05-02 16:21:42 +0300

    Added addcontact and delete contact

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React, {Fragment} from 'react';
+import './App.css';
+import Navbar from "./components/layout/Navbar"; 
+import Home from "./components/pages/Home"; 
+import About from "./components/pages/About"; 
+import ContactState from "./context/contact/contactState"; 
+import {BrowserRouter as Router, Route, Switch, Link} from "react-router-dom"; 
+
+function App() {
+  return (
+    <ContactState> 
+    <Router> 
+    <Fragment> 
+    <Navbar /> 
+  
+      <div className="container"> 
+      <Switch> 
+        <Route exact path="/" component={Home} /> 
+        <Route exact path="/about" component={About} /> 
+
+
+      </Switch>
+      </div>
+      </Fragment>
+
+    </Router>
+    </ContactState>
+
+  );
+}
+
+export default App;
---
+++ client/src/context/contact/contactState.js
@@ -0,0 +1,93 @@
+import React, {useReducer } from "react"; 
+
+import {v4 as uuidv4} from "uuid"; 
+import ContactContext from "./contactContext";
+import contactReducer from "./contactReducer"; 
+
+import { 
+    ADD_CONTACT,
+    DELETE_CONTACT,
+    SET_CURRENT,
+    CLEAR_CURRENT,
+    UPDATE_CONTACT,
+    FILTER_CONTACTS,
+    CLEAR_FILTER
+} from "../types"; 
+
+const ContactState = props => {
+    const initialState = {
+        contacts: [
+            {
+                id: 1, 
+                name: "Jill Johnson",
+                email: "jill@gmail.com",
+                phone: "111-111-111",
+                type: "personal" 
+
+            },
+            {
+                id: 2, 
+                name: "Sarah Smith",
+                email: "sarah@gmail.com",
+                phone: "111-231-111",
+                type: "personal" 
+
+            },
+            {
+                id: 3, 
+                name: "Matt Jacobs",
+                email: "matthew@gmail.com",
+                phone: "133-111-111",
+                type: "professional" 
+
+            },
+            {
+                id: 4, 
+                name: "Morgan Thomas",
+                email: "morgan1@gmail.com",
+                phone: "111-111-1241",
+                type: "professional" 
+
+            }
+        ]
+        }; 
+    
+    
+        const [state, dispatch] = useReducer(contactReducer, initialState); 
+
+        // Add contact 
+
+        const addContact = contact => {
+            contact.id = uuidv4(); 
+            dispatch({ type: ADD_CONTACT, payload: contact }); 
+        }
+
+        // Delete contact 
+
+        const deleteContact = id => {
+            dispatch({type: DELETE_CONTACT, payload: id })
+        }
+
+        // Set Current contact 
+
+        // Clear current contact 
+
+        // Update contact 
+
+        // Filter contacts 
+
+        // Clear filter 
+
+        return (
+            <ContactContext.Provider
+            value= {{
+                contacts: state.contacts,
+                addContact,
+                deleteContact
+            }}> 
+                { props.children }
+            </ContactContext.Provider>
+        )
+    } 
+
+        export default ContactState; 
