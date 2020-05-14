efff76ec75253baa719c403fd9a91772fc145e14
Minja Sillanpää <minja@vainu.io>
2020-05-14 15:27:37 +0300

    Finalized project with update contacts CRUD and styling

--- client/src/context/contact/contactState.js
+++ client/src/context/contact/contactState.js
@@ -89,7 +89,29 @@ const ContactState = props => {
                 
             }
 
-        }  ;
+        };
+
+         // Update contact 
+         const updateContact = async contact => {
+             const config= {
+             headers: {
+                 "Content-Type": "application/json"
+             }}; 
+
+             try {
+                 const res = await axios.put(`/api/contacts/${contact._id}`, contact, config); 
+
+                 dispatch({
+                     type: UPDATE_CONTACT,
+                     payload: res.data 
+                 })
+             } catch (err) {
+                 dispatch({
+                     type: CONTACT_ERROR, 
+                     payload: err.response.msg 
+                 }); 
+             }
+        }; 
 
           // Clear contacts 
 
@@ -106,10 +128,6 @@ const ContactState = props => {
         // Clear current contact 
         const clearCurrent = () => {
             dispatch({type: CLEAR_CURRENT})
-        }
-        // Update contact 
-        const updateContact = contact => {
-            dispatch({type: UPDATE_CONTACT, payload: contact})
         }
 
 
